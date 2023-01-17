a35b610d4f551f5fbb3cf3afe9f2e5f224507a0d
Thirhag <meigraf@hotmail.de>
2023-01-17 10:58:03 +0100

    2

--- script_01.js
+++ script_01.js
@@ -2,4 +2,26 @@
 
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
+// Funktionsaufruf (call)
+// test();
+// test();
+// test();
+// test();
+// test();
+// test();
 
+// Funktionsrumpf (body)| callee
+// Funktionsdeklaration
+function test()
+
+
+/****** Funktionen 02a ************/
+// 2a. Parametrisierung + Datenübergabe vin INNEN
+
+ausgabeNamen();
+console.log(firstName); // Fehler Scope!
+
+function ausgabeNamen() {
+   let firstName = "Martin";
+   console.log("Hallo" + firstName + "!")
+}
