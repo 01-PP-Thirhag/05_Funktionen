28292e2c15ff91ec333b6a3c16f6149e623e2b96
Thirhag <meigraf@hotmail.de>
2023-01-17 14:19:57 +0100

    3b

--- script_01.js
+++ script_01.js
@@ -7,7 +7,7 @@
 
 // Funktionsrumpf (body)| callee
 // Funktionsdeklaration
-function test(){}
+// function test(){}
 
 /****** Funktionen 02a ************/
 // 2a. Parametrisierung + Datenübergabe von INNEN
@@ -15,10 +15,10 @@ function test(){}
 // ausgabeNamen();
 // console.log(firstName); // Fehler Scope!
 
-function ausgabeNamen() {
+// function ausgabeNamen() {
 //    let firstName = "Martin";
 //    console.log("Hallo" + " " + firstName + "!")
-}
+// }
 
 /***** Funktionen 02a *****/
 // 2a. Parametrisierung + Datenübergabe von INNEN
@@ -26,10 +26,10 @@ function ausgabeNamen() {
 // ausgabeNamen();
 // console.log(firstName); // Fehler: Scope!
 
-function ausgabeNamen() {
-    let firstName = "Heinrich"; // what happens in Vegas, ...
-    console.log("Hallo " + firstName + "!");
-}
+// function ausgabeNamen() {
+//     let firstName = "Heinrich"; // what happens in Vegas, ...
+//     console.log("Hallo " + firstName + "!");
+// }
 
 /***** Funktionen 02b *****/
 // 2b. Parametrisierung + Datenübergabe von AUSSEN
@@ -38,9 +38,9 @@ function ausgabeNamen() {
 // ausgabeNamenParam("Lawrence");
 // Argumente sind Daten für Parameter
 
-function ausgabeNamenParam(firstName) { //Funktion + Parameter
-    console.log("Hallo " + firstName + "!");
-}
+// function ausgabeNamenParam(firstName) { //Funktion + Parameter
+//     console.log("Hallo " + firstName + "!");
+// }
 
 /***** Funktionen 02c *****/
 // 2c. Mehrere Parameter
@@ -50,9 +50,9 @@ function ausgabeNamenParam(firstName) { //Funktion + Parameter
 // ausgabeNamenParams("Max","Mütze");
 // ausgabeNamenParams(prompt("Vorname?"),prompt("Nachname?"));
 
-function ausgabeNamenParams(firstName, familyName) {
-    console.log("Hallo " + firstName + " " + familyName +"!");
-}
+// function ausgabeNamenParams(firstName, familyName) {
+//     console.log("Hallo " + firstName + " " + familyName +"!");
+// }
 
 /***** Funktionen 03a *****/
 // 03a. Vorbereitung
@@ -64,11 +64,11 @@ function ausgabeNamenParams(firstName, familyName) {
 function ausgabeNamenParams2(firstName, familyName) {
 
     // 1. Funktionalität: string composing
-    const GAP = " ";
-    let outputStr = "Hallo" + GAP + firstName + GAP + familyName +"!";
+    // const GAP = " ";
+    // let outputStr = "Hallo" + GAP + firstName + GAP + familyName +"!";
 
     // 2. Funktionalität: string output
-    console.log(outputStr);
+    // console.log(outputStr);
 }
 
 /***** Funktionen 03b *****/
@@ -78,7 +78,8 @@ function ausgabeNamenParams2(firstName, familyName) {
 // 1. Funktionalität: string composing
 output(getString("Max", "Mütze"));
 
-function getString(firstName,familyName){
+function getString(firstName,familyName)
+{
     const GAP = " ";
     let outputStr = "Hallo" + GAP + firstName + GAP + familyName + "!";
 }
